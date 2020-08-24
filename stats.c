15213578ae851a98ec97ca8082d86fdfd4ad9b26
MohamadWahba <momowehba123@gmail.com>
2020-08-24 02:45:23 +0200

    "first try"

--- stats.c
+++ stats.c
@@ -24,10 +24,15 @@
 
 
 #include <stdio.h>
+#include <stdlib.h>
 #include "stats.h"
 
 /* Size of the Data Set */
 #define SIZE (40)
+unsigned int i = 0;
+int max;
+int min;
+
 
 void main() {
 
@@ -36,10 +41,114 @@ void main() {
                               200, 122, 150, 90,   92,  87, 177, 244,
                               201,   6,  12,  60,   8,   2,   5,  67,
                                 7,  87, 250, 230,  99,   3, 100,  90};
+	
 
-  /* Other Variable Declarations Go Here */
+
   /* Statistics and Printing Functions Go Here */
-
+	
+	
+	print_statistics(SIZE , test);
+	
+	print_array(SIZE , test);
 }
 
 /* Add other Implementation File Code Here */
+
+
+//find_maximum() implementation
+int find_maximum( unsigned int arr_size, unsigned char *array ){
+
+	max = array[0]; //assume the max is the first item 
+
+	for(i = 0 ; i<arr_size ; i++){
+	
+	if(array[i]>max){ max = array[i]; } //compare each item with max and change max accordingly till end of array
+		
+	}
+
+	return max;
+}
+/**************************************************************/
+
+//find_minimum() implementation
+int find_minimum( unsigned int arr_size, unsigned char *array ){
+
+	min = array[0]; //assume the min is the first item 
+
+	for(i = 0 ; i<arr_size ; i++){
+	
+	if(array[i]<min){ min = array[i]; } //compare each item with min and change min accordingly till end of array
+		
+	}
+
+	return min;
+}
+/**************************************************************/
+
+//find_mean() implementation (i.e the sum of the elements by its number )
+int find_mean( unsigned int arr_size, unsigned char *array ){
+
+	int sum = 0; //initialize sum with 0 (a container)  
+
+	for(i = 0 ; i<arr_size ; i++){
+	
+		sum+= array[i];
+	}
+
+	return (sum/arr_size); //sum of the elements(sum) by its number(arr_size)
+}
+/**************************************************************/
+
+//find_median() implementation 
+int find_median(unsigned int arr_size ,unsigned char  *array){
+
+	array = sort_array(arr_size, array); 
+
+	if(arr_size % 2 != 0) //if the array is odd then the middle is the median
+		return array[arr_size/2];
+	return (array[(arr_size-1)/2] + array[arr_size/2])/2; // if even take average of 2 center values 
+}
+
+/**************************************************************/
+
+//sort_array() implementation
+unsigned char* sort_array(unsigned int arr_size ,unsigned char  *array ){
+	for(int i = arr_size-1 ; i>=0 ;i--){
+		for(int j = 0 ; j<i ; j++){
+	
+			if(array[j] > array[j+1]){
+				
+				unsigned char temp = array[j];
+				array[j] = array[j+1];
+				array[j+1] = temp;
+			}		
+		}
+		
+	}
+	
+	return array;
+}
+
+/**************************************************************/
+
+//print_array() implementation
+ void print_array(unsigned int arr_size ,unsigned char  *array){
+	
+	printf("%s","sorted array :  ");
+	for(i=0 ; i<arr_size ; i++){
+		
+		printf("%d,  ",array[i]);
+	}
+	printf("%s","\n");
+}
+/**************************************************************/
+
+//print_statistics() implementation
+void print_statistics(unsigned int arr_size ,unsigned char  *array){
+
+	printf("\nMaximum value = %d \n",find_maximum(arr_size , array));
+	printf("minimum value = %d \n",find_minimum(arr_size , array));
+	printf("Mean value = %d \n",find_mean(arr_size , array));
+	printf("Median value = %d \n\n",find_median(arr_size , array));
+}
+ 
--- stats.h
+++ stats.h
@@ -12,7 +12,8 @@
  * @file stats.h
  * @brief the header file of the project containing all function declarations 
  *
- *
+ * this is the header file containing all the function declarations 
+ * 
  * @author Mohamad Wahba
  * @date 23/8/2020
  *
@@ -23,17 +24,108 @@
 /* Add Your Declarations and Function Comments here */ 
 
 /**
- * @brief <Add Brief Description of Function Here>
+ * @brief   a function to print the content of the array sequentially 
  *
- * <Add Extended Description Here>
+ * 
  *
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
+ * @param   arr_size   an unsigned integer representing the size of the array (i.e the number of elements) 
+ * 
+ * @param  *array   unsigned pointer to the array 
  *
- * @return <Add Return Informaiton here>
+ * @return  void no return type
  */
+
+void print_array(unsigned int arr_size ,unsigned char  *array);
+
+
+
+/**
+ * @brief   a function to print all the statistics done on the array 
+ *
+ * 
+ *
+ * @param   arr_size   an unsigned integer representing the size of the array (i.e the number of elements) 
+ * 
+ * @param  *array   unsigned pointer to the array 
+ *
+ * @return  void no return type
+ */
+void print_statistics(unsigned int arr_size ,unsigned char  *array);
+
+
+
+/**
+ * @brief   a function to get the maximum value in the given array 
+ *
+ * 
+ *
+ * @param   arr_size   an unsigned integer representing the size of the array (i.e the number of elements) 
+ * 
+ * @param  *array   unsigned pointer to the array 
+ *
+ * @return max  an integer value of the maximum
+ */
+int find_maxumum(unsigned int arr_size ,unsigned char  *array);
+
+
+
+/**
+ * @brief   a function to get the minimum value in the given array 
+ *
+ * 
+ *
+ * @param   arr_size   an unsigned integer representing the size of the array (i.e the number of elements) 
+ * 
+ * @param  *array   unsigned pointer to the array 
+ *
+ * @return min  integer value of the maximum
+ */
+int find_minimum(unsigned int arr_size ,unsigned char  *array);
+
+
+/**
+ * @brief   a function to get the mean value in the given array (which is basically an average)
+ *
+ * 
+ *
+ * @param   arr_size   an unsigned integer representing the size of the array (i.e the number of elements) 
+ * 
+ * @param  *array   unsigned pointer to the array 
+ *
+ * @return (sum/arr_size) integer value of the mean (sum of the elements / their number )
+ */
+int find_mean(unsigned int arr_size ,unsigned char  *array);
+
+
+/**
+ * @brief   a function to get the median value in the given array (which is not sorted, so we need to sort first)
+ *
+ * the median of an odd num of elements is the middle, and the average of the two center values if even 
+ *
+ * @param   arr_size   an unsigned integer representing the size of the array (i.e the number of elements) 
+ * 
+ * @param  *array   unsigned pointer to the array 
+ *
+ * @return (array[(arr_size-1)/2] + array[arr_size/2])/2    integer value of the median (sum of the elements / their number )
+ */
+int find_median(unsigned int arr_size ,unsigned char  *array);
+
+
+
+
+/**
+ * @brief   a bubble sort function
+ *
+ * the median of an odd num of elements is the middle, and the average of the two center values if even 
+ *
+ * @param   arr_size   an unsigned integer representing the size of the array (i.e the number of elements) 
+ * 
+ * @param  *array   unsigned pointer to the array 
+ *
+ * @return (array[(arr_size-1)/2] + array[arr_size/2])/2    integer value of the median (sum of the elements / their number )
+ */
+
+unsigned char* sort_array(unsigned int arr_size ,unsigned char  *array);
 
 
 #endif /* __STATS_H__ */
